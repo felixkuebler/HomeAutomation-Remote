4bf4ae5403ae3c8e35aed9e6b374e43a95029e57
felixkuebler <felix.kuebler@variex.de>
2023-05-06 22:00:34 +0200

    add software first draft

---
+++ Software/Source/RemoteController/BOKeypad.hpp
@@ -0,0 +1,89 @@
+
+
+
+enum BtnId: uint8_t {
+  TV,
+  LIGHT,
+  RADIO,
+  DTV,
+  DVD,
+  CD,
+  VMEM,
+  RECORD,
+  AMEM,
+  NUM7,
+  NUM8,
+  NUM9,
+  NUM4,
+  NUM5,
+  NUM6,
+  NUM1,
+  NUM2,
+  NUM3,
+  TEXT,
+  NUM0,
+  MENU,
+  GREEN,
+  UP,
+  YELLOW,
+  LEFT,
+  GO,
+  RIGHT,
+  RED,
+  DOWN,
+  BLUE,
+  LIST,
+  VOLUP,
+  EXIT,
+  STOP,
+  VOLDOWN,
+  OFF,
+  NUM_IDS,
+  NONE = 255
+};
+
+char* btnNames[BtnId::NUM_IDS] = {
+  "TV", "LIGHT", "RADIO", "DTV", "DVD", "CD", "VMEM", "RECORD", "AMEM", "7", "8", "9", "4", "5", "6", "1", "2", "3",
+  "TEXT", "0", "MENU", "GREEN", "UP", "YELLOW", "LEFT", "GO", "RIGHT", "RED", "DOWN", "BLUE", "LIST", "VOL UP", "EXIT", "STOP", "VOL DOWN", "OFF"
+};
+
+constexpr uint8_t numGpioOut = 6;
+constexpr uint8_t numGpioIn = 6;
+
+BtnId btnIds[numGpioOut][numGpioIn] = {
+  {BtnId::TV, BtnId::DTV, BtnId::VMEM, BtnId::NUM7, BtnId::NUM4, BtnId::NUM1},
+  {BtnId::LIGHT, BtnId::DVD, BtnId::RECORD, BtnId::NUM8, BtnId::NUM5, BtnId::NUM2},
+  {BtnId::RADIO, BtnId::CD, BtnId::AMEM, BtnId::NUM9, BtnId::NUM6, BtnId::NUM3},
+  {BtnId::TEXT, BtnId::GREEN, BtnId::LEFT, BtnId::RED, BtnId::LIST, BtnId::STOP},
+  {BtnId::NUM0, BtnId::UP, BtnId::GO, BtnId::DOWN, BtnId::VOLUP, BtnId::VOLDOWN},
+  {BtnId::MENU, BtnId::YELLOW, BtnId::RIGHT, BtnId::BLUE, BtnId::EXIT, BtnId::OFF}
+};
+
+BtnId gpioToBtnId(uint8_t outPin, uint8_t inPin){
+  if (outPin < numGpioOut && inPin < numGpioIn) {
+    return btnIds[outPin][inPin];
+  }
+  return BtnId::NONE;
+}
+
+char* getBtnName(BtnId id) {
+  if (id < BtnId::NUM_IDS) {
+    return btnNames[id];
+  }
+  return nullptr;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Software/Source/RemoteController/RemoteController.ino
+++ Software/Source/RemoteController/RemoteController.ino
@@ -1,12 +1,95 @@
+#include <stdio.h>
 
-void setup(void)
-{
+#include "SPI.h"
+
+#include "MCP23S17.h"
+
+#include "epd1in54_V2.h"
+#include "imagedata.h"
+#include "epdpaint.h"
+
+#include "BOKeypad.hpp"
+
+const uint8_t expanderCs = 15;
+const uint8_t displayCs = 14;
+
+MCP23S17 expander(expanderCs);
+Epd display;
+
+unsigned char image[1024];
+Paint paint(image, 0, 0);
+
+#define COLORED     0
+#define UNCOLORED   1
+
+
+
+void setup() {
+  Serial.begin(9600);
+  delay(100);
+
+  SPI.begin();
+  delay(100);
+
+  setupGpioExpander();
+  delay(100);
+
+  setupDisplay();
+  delay(100);
+}
+
+bool setupGpioExpander() {
+  expander.begin();
+
+  expander.pinMode(0, OUTPUT);
+  expander.pinMode(1, OUTPUT);
+  expander.pinMode(2, OUTPUT);
+  expander.pinMode(3, OUTPUT);
+  expander.pinMode(4, OUTPUT);
+  expander.pinMode(5, OUTPUT);
+  expander.pinMode(8, INPUT);
+  expander.pinMode(9, INPUT);
+  expander.pinMode(10, INPUT);
+  expander.pinMode(11, INPUT);
+  expander.pinMode(12, INPUT);
+  expander.pinMode(13, INPUT);
   
+  return expander.isConnected();
 }
 
-void loop(void)
-{
+bool setupDisplay() {
+  display.LDirInit();
+  display.Clear();
 
+  return true; 
+}
+
+void loop() {
+  for (int outPin = 0; outPin < 6; outPin++) {
+    expander.digitalWrite(outPin, 1);
+    for (int inPin = 0; inPin < 6; inPin++) {
+      bool assert = expander.digitalRead(inPin+8);
+      if (assert) {
+        BtnId id = gpioToBtnId(outPin, inPin);
+        Serial.println(getBtnName(id));
+        displayText(getBtnName(id));
+      }
+    }
+    expander.digitalWrite(outPin, 0);
+  }
+
+
+  delay(100);
 }
 
 
+void displayText(char* text) {
+  paint.SetWidth(200);
+  paint.SetHeight(24);
+
+  paint.Clear(UNCOLORED);
+  paint.DrawStringAt(30, 4, text, &Font16, COLORED);
+
+  display.SetFrameMemoryPartial(paint.GetImage(), 0, 0, paint.GetWidth(), paint.GetHeight());
+  display.DisplayPartFrame();
+}
